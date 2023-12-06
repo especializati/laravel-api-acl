d65874ec8aa4d2b564cd6b59937903c067255927
Carlos Ferreira <carlos@especializati.com.br>
2023-12-06 09:43:51 -0300

    refactor: convert project to api

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/', fn () => response()->json(['message' => 'ok']));
