649fe1af76b3f8caf02786c53b593d496aa50261
Carlos Ferreira <carlos@especializati.com.br>
2023-12-06 09:44:08 -0300

    feat: list users with pagination

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -3,10 +3,22 @@
 namespace App\Repositories;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class UserRepository
 {
     public function __construct(protected User $user)
     {   
     }
+
+    public function getAllUsers(string $filter = ''): LengthAwarePaginator
+    {
+        return $this->user->where(function ($query) use ($filter) {
+            if ($filter !== '') {
+                $query->where('name', 'LIKE', "%{$filter}%");
+            }
+        })->paginate();
+    }
+
 }
