e79a61c3f89a98050dff468214e75aee7935b5f7
Carlos Ferreira <carlos@especializati.com.br>
2023-12-08 10:09:15 -0300

    refactor: apply middleware auth sanctum

--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,14 @@ Route::post('/me', [AuthApiController::class, 'me'])->name('auth.me')->middlewar
 Route::post('/logout', [AuthApiController::class, 'logout'])->name('auth.logout')->middleware('auth:sanctum');
 Route::post('/auth', [AuthApiController::class, 'auth'])->name('auth.login');
 
-Route::apiResource('/permissions', PermissionController::class);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('/permissions', PermissionController::class);
 
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+});
 
 Route::get('/', fn () => response()->json(['message' => 'ok']));
